a0bb0ad21aba1b78c97855dc86702f083cb6e837
Ilya Zadorozhny <izadorojniy@yandex.ru>
2015-08-17 23:06:21 +0300

    string test

--- src/foo/foo.h
+++ src/foo/foo.h
@@ -1,7 +1,9 @@
 #ifndef CPP_FOO_H
 #define CPP_FOO_H
 
-void hello_world(int& refVal, int* pVal);
+void hello_world(int& refVal, int* pVal, int rVal);
+
+int stringLength(const wchar_t* st);
 
 
 #endif //CPP_FOO_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
+#include <string>
 #include "foo/foo.h"
 #include "Sales_item.h"
+
+//using namespace std;
+using std::wstring;
 
 int main(int argc, char** argv) {
 	int val1 = 6;
 	int val2 = 8;
+	int val4 = 10;
 
 	int* val3 = nullptr;
 
 	std::cout << val1 << std::endl;
 	std::cout << val2 << std::endl;
+	std::cout << val4 << std::endl;
 
-	hello_world(val1, &val2);
+	hello_world(val1, &val2, val4);
 
 	std::cout << val1 << std::endl;
 	std::cout << val2 << std::endl;
 	std::cout << val3 << std::endl;
+	std::cout << val4 << std::endl;
+
+	const wchar_t* st = L"Цена бутылки вина";
+	wstring st2 = L"Цена бутылки вина";
+	std::cout << st << std::endl;
+	int len = stringLength(st);
+
+	std::cout << len << std::endl;
+    //std::cout << st << std::endl;
+
+    std::cout << st2.size() << std::endl;
 
 //	int sum = 0;
 //	int i;
