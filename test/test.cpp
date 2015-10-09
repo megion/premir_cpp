9e2d000aa928aff2e3aea9436505bd3e6a9c0279
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2015-10-09 19:21:24 +0300

    test

--- test/test.cpp
+++ test/test.cpp
@@ -15,54 +15,10 @@ static void test_size_string() {
 	assert(buf.size() == 6);
 }
 
-//static void test_linked_list_create() {
-//	utils::LinkedList<int> list;
-//	list.push(1);
-//	list.push(2);
-//	assert(list.size() == 2);
-//
-//	utils::LinkedList<int> list2;
-//	list2.push(3);
-//	assert(list2.size() == 1);
-//
-//	list.concat(list2);
-////	std::cout<<"size:"<< list.size() << std::endl;
-//	assert(list.size() == 3);
-//
-//	utils::LinkedList<int> list3(list);
-//	assert(list3.size() == 3);
-//}
-//
-//static void test_linked_list_iterator() {
-//	utils::LinkedList<int> list;
-//	list.push(10);
-//	list.push(20);
-//	list.push(30);
-//	int i = 0;
-//	// using while cycle
-//	utils::LinkedListIterator<int> iter = list.iterator();
-//	while(iter.hasNext()) {
-//		i++;
-//		utils::LinkedListItem<int>* item = iter.next();
-////		std::cout<<"item" << i << ": " << item->getValue() << std::endl;
-//	}
-//	assert(i == 3);
-//
-//	// using for cycle
-//	utils::LinkedListIterator<int> iter2 = list.iterator();
-//	i = 0;
-//	for (;iter2.hasNext();) {
-//		i++;
-//		utils::LinkedListItem<int>* item = iter2.next();
-////		std::cout<<"item2" << i << ": " << item->getValue() << std::endl;
-//	}
-//	assert(i == 3);
-//}
-
 int main(int argc, char** argv) {
 
-//	suite("String");
-//	test(size_string);
+	suite("String");
+	test(size_string);
 
 //	suite("LinkedList");
 //	test(linked_list_create);
