d2812eeaeb4a2d5eab032399c192d2b20405dff8
Ilya Zadorozhny <izadorojniy@yandex.ru>
2016-02-09 22:56:42 +0300

    sammon

--- src/kohonen/SammonMap.h
+++ src/kohonen/SammonMap.h
@@ -17,6 +17,7 @@
 
 #include "utils/SMatrix.h"
 #include "utils/RMatrix.h"
+#include "utils/ArrayUtils.h"
 #include "file/stream/StreamReader.h"
 #include "kohonen/alphafunc/AlphaFunction.h"
 #include "kohonen/neighadap/NeighborAdaptation.h"
@@ -24,21 +25,41 @@
 
 namespace kohonen {
 
-    template<typename In, typename Out>
+    template<typename Out>
     class SammonMap {
     public:
 
         typedef utils::RMatrix<models::NeuronInfo, Out> OutCodes;
+        typedef utils::RMatrix<models::NeuronInfo, Out>::Row Neuron;
+        typedef utils::ArrayUtils<Out, Out, Out> ArrayUtils;
 
         SammonMap(){
+            randomEngine = new RandomGenerator();
+        }
+
+        ~SammonMap() {
+            delete randomEngine;
         }
 
         /**
          * Построение проекций Саммона
          */
-        bool training(OutCodes *initializedSom,
+        bool sammon(OutCodes *trainedSom,
                       size_t iterationLen) {
 
+            setSkippedIdenticalNeurons(trainedSom);
+
+            long neuronsLen = trainedSom->getRowSize();
+            Out x[neuronsLen];
+            Out y[neuronsLen];
+            Out xu[neuronsLen];
+            Out yu[neuronsLen];
+            Out dd[neuronsLen*(neuronsLen-1)/2];
+
+            for (size_t i = 0; i < neuronsLen; ++i) {
+                x[i] = (Out) (randomEngine->generate() % neuronsLen) / neuronsLen;
+                y[i] = (Out) (i) / neuronsLen;
+            }
 
             for (size_t le = 0; le < iterationLen; ++le) {
 
@@ -46,21 +67,35 @@ namespace kohonen {
             return true;
         }
 
-        bool sammonIterationSample(OutCodes *trainedSom,
-                              size_t iterationLen,
-                              size_t index) {
-        }
+
 
         /**
          * Для идентичных нейронов (евклидовое расстояние между котороми
          * = 0) установить флаг skipped = true.
          */
         void setSkippedIdenticalNeurons(OutCodes *trainedSom) {
+            size_t colSize = trainedSom->getColSize();
+            for (size_t i = 0; i < trainedSom->getRowSize(); ++i) {
+                Neuron &ni = trainedSom->getRow(i);
+                ni.data.skipped = false; // сбросить флаг в начале
+                for (size_t j = i + 1; j < trainedSom->getRowSize(); ++j) {
+                    Neuron &nj = trainedSom->getRow(j);
+                    if (ArrayUtils::euclideanDistance(
+                            ni.points, nj.points, colSize) == 0) {
+                        ni.data.skipped = true;
+                        break;
+                    }
+                }
+            }
+        }
 
+        RandomGenerator *getRandomGenerator() const {
+            return randomEngine;
         }
 
     private:
-
+        // генератор псевдо случайных чисел
+        RandomGenerator *randomEngine;
     };
 }
 
