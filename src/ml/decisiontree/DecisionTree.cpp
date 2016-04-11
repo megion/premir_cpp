16d51920092bc15fc6941dd1a0feb5101d121476
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-04-11 19:02:56 +0300

    decision tree 1

---
+++ src/ml/decisiontree/DecisionTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilya on 4/11/16.
+//
+
+#include "DecisionTree.h"
---
+++ src/ml/decisiontree/DecisionTree.h
@@ -0,0 +1,19 @@
+//
+// Created by ilya on 4/11/16.
+//
+
+#ifndef SRC_ML_DECISIONTREE_DECISIONTREE_H
+#define SRC_ML_DECISIONTREE_DECISIONTREE_H
+
+namespace ml {
+    namespace decisiontree {
+
+        class DecisionTree {
+
+        };
+    }
+}
+
+
+
+#endif
