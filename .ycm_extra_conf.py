958c88b33a95d8541e80d95eaa665ee7547e2785
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-11-23 19:35:38 +0300

    test

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -43,9 +43,6 @@ flags = [
 '-Wno-variadic-macros',
 '-fexceptions',
 '-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
@@ -59,28 +56,8 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-# '-isystem',
-# '../BoostParts',
-# '-isystem',
-# # This path will only work on OS X, but extra paths that don't exist are not
-# # harmful
-# '/System/Library/Frameworks/Python.framework/Headers',
-# '-isystem',
-# '../llvm/include',
-# '-isystem',
-# '../llvm/tools/clang/include',
-'-I',
-'.',
-# '-I',
-# './ClangCompleter',
-# '-isystem',
-# './tests/gmock/gtest',
-# '-isystem',
-# './tests/gmock/gtest/include',
-# '-isystem',
-# './tests/gmock',
-# '-isystem',
-# './tests/gmock/include',
+'-I./src',
+'-I./test'
 ]
 
 
