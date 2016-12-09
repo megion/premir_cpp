b3e34b5dee85bef1953526943616f59aa27a5721
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-12-09 18:06:33 +0300

    generate test

--- test/comb/CombinatoricsTest.cpp
+++ test/comb/CombinatoricsTest.cpp
@@ -24,29 +24,46 @@ namespace test {
 		void test_generate_symbols() {
 			size_t totalLen = 800;
 			size_t lineLen = 80;
+			size_t wordLen = 8;
 			char letters[] = {'q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p',
 				'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
 				'z', 'x', 'c', 'v', 'b', 'n', 'm'
 			};
-            char apperLetters[] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+            char upperLetters[] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 				'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
 				'Z', 'X', 'C', 'V', 'B', 'N', 'M'
 			};
 			size_t lettersCount = 26;
 			char symbols[] = {';', ':'};
 			size_t symbolsCount = 2;
-			double apperLettersProb = 0.2;
+			size_t upperLettersFreq = 6; // frequancy apper upper letter 
 
 			std::random_device rd;     // only used once to initialise (seed) engine
 			std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-			std::uniform_int_distribution<size_t> uni(0, lettersCount); // guaranteed unbiased
+			std::uniform_int_distribution<size_t> uni(0, lettersCount-1); // guaranteed unbiased
+			std::uniform_int_distribution<size_t> upperFreqRandom(0, upperLettersFreq-1); // guaranteed unbiased
 			
-			std::default_random_engine generator;
-			std::normal_distribution<double> distribution(0.0, 0.1);
+			//std::default_random_engine generator;
+			//std::normal_distribution<double> distribution(0.0, 0.1);
 			for (size_t i = 0; i < totalLen; i++) {
-				//std::cout<<uni(rng)<<std::endl;
-			}
+				size_t lettersIndex = uni(rng);
+				size_t uLetterIndex = uni(rng);
+				bool isNewLine = false;
+				if ((i%lineLen == 0) && (i != 0)) {
+					std::cout << std::endl; // end of line
+					isNewLine = true;
+				}
+				if ((i%wordLen == 0) && (i != 0) && !isNewLine) {
+					std::cout << " "; // end of word 
+				}
 
+				if (upperFreqRandom(rng)==0) {
+					std::cout << upperLetters[uLetterIndex];
+				}
+				
+				std::cout<<letters[lettersIndex];
+			}
+			std::cout << std::endl; // end of line
 		}
 
 		void combinatorics_test() {
