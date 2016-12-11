4e367d42ef9ed2cc5dcb753f9a95d04cb77070b0
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-12-12 00:04:07 +0300

    color changes

--- test/test.h
+++ test/test.h
@@ -4,12 +4,30 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 /*
  * Test the given `fn`.
  */
 
 #define mytest(fn) \
-  printf("    \e[92m✓ \e[90m%s\e[0m\n", #fn); \
+  printf("    " GREEN "✓ %s" RESET "\n", #fn); \
   test_##fn();
 
 /*
@@ -17,7 +35,7 @@
  */
 
 #define suite(title) \
-  printf("\n  \e[36m%s\e[0m\n", title)
+  printf("\n  " CYAN "%s" RESET "\n", title)
 
 /*
  * Report sizeof.
