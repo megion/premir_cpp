206d0a6d7577c0f321a44b6b0bf452ddbbbd1e58
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-12-19 18:23:59 +0300

    array hash init

---
+++ src/utils/hash/ArrayHash.h
@@ -0,0 +1,34 @@
+//
+// Created by ilya on 1/6/16.
+// hash code for types:
+// 1) integer value
+// 2) float number value if float range number is not defined
+//
+
+#ifndef SRC_UTILS_HASH_NUMBERHASH_H
+#define SRC_UTILS_HASH_NUMBERHASH_H
+
+#include <cstdio>
+
+#include "HashEngine.h"
+
+namespace utils {
+    namespace hash {
+
+        template<typename T>
+        class NumberHash : public HashEngine<T> {
+        public:
+            NumberHash() : HashEngine<T>::HashEngine() {
+            }
+
+            size_t hashCode(const T& value) const {
+                return ((size_t)value) % HashEngine<T>::indexSize;
+            }
+
+
+        };
+    }
+}
+
+
+#endif
