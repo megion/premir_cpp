2485e581b4276dda556874d64e49d3a1ff986659
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-12-19 23:37:07 +0300

    dek hash

--- src/utils/hash/HashEngine.h
+++ src/utils/hash/HashEngine.h
@@ -2,6 +2,7 @@
 #define SRC_UTILS_HASH_HASHENGINE_H
 
 #include <cstdio>
+#include <cstring>
 
 namespace utils {
     namespace hash {
@@ -10,7 +11,8 @@ namespace utils {
         class HashEngine {
         public:
 
-            HashEngine(size_t _indexSize=DEFAULT_TABLE_SIZE) : indexSize(_indexSize) {
+            HashEngine(size_t _indexSize=DEFAULT_TABLE_SIZE) : indexSize(_indexSize),
+			doubleSizeof(sizeof(double)) {
             }
 
             virtual size_t hashCode(const T& value) const = 0;
@@ -19,8 +21,31 @@ namespace utils {
                 indexSize = _indexSize;
             }
 
+			size_t hash(const double& value) {
+				char temp[doubleSizeof];
+				std::memcpy(temp, &value, doubleSizeof);
+			}
+
         protected:
             size_t indexSize;
+
+			size_t doubleSizeof;
+			
+			/**
+			 * An algorithm proposed by Donald E. Knuth in The Art Of Computer Programming Volume 3,
+			 * under the topic of sorting and search chapter 6.4.
+			 */
+			size_t DEKHash(const char* str, const size_t& len) {
+				size_t hash = len;
+				for(size_t i = 0; i < len; str++, i++) {
+					hash = ((hash << 5) ^ (hash >> 27)) ^ (*str);
+				}
+				return hash;
+			}
+			
+			size_t strHash(const char* str, const size_t& len) {
+				return DEKHash(str, len);
+			}
 
         public:
             // наилучшее случайное распределние занений хэш функции получается,
