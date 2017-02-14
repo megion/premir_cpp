6740fc13b91721fb20b1aa9589e616f5ba300999
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-02-14 23:38:25 +0300

    test

--- src/cache/LinkedHashMap.h
+++ src/cache/LinkedHashMap.h
@@ -106,18 +106,17 @@ namespace cache {
 			typedef int (*hashmap_cmp_fn)(const hashmap_entry<T> *entry,
 				   	const hashmap_entry<T> *entry_or_key, const void *keydata);
 			
-			LinkedHashMap(hashmap_cmp_fn equals_function, size_t initial_size) {
-				unsigned int size = HASHMAP_INITIAL_SIZE;
-				size = 0;
+			LinkedHashMap(hashmap_cmp_fn equals_function, size_t initial_size) : size(0) {
+				unsigned int _size = HASHMAP_INITIAL_SIZE;
 				cmpfn = equals_function ? equals_function : always_equal;
 
 				/* calculate initial table size and allocate the table */
 				initial_size = (unsigned int) ((uint64_t) initial_size * 100
 						/ HASHMAP_LOAD_FACTOR);
-				while (initial_size > size) {
-					size <<= HASHMAP_RESIZE_BITS;
+				while (initial_size > _size) {
+					_size <<= HASHMAP_RESIZE_BITS;
 				}
-				alloc_table(size);
+				alloc_table(_size);
 			}
 
 			/*
