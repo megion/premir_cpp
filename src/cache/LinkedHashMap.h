2168afb37899c41613647b7fbcc8a8343ba930e7
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-02-14 19:31:48 +0300

    linked hash map

--- src/cache/LinkedHashMap.h
+++ src/cache/LinkedHashMap.h
@@ -81,6 +81,9 @@ namespace cache {
 	/* data structures */
 	template<typename T>
 	struct hashmap_entry {
+		hashmap_entry(unsigned int _hash): hash(_hash), next(nullptr) {
+		}
+
 		struct hashmap_entry<T> *next;
 		unsigned int hash;
 		T value;
@@ -117,139 +120,23 @@ namespace cache {
 				alloc_table(size);
 			}
 
-			~LinkedHashMap() {
-				hashmap_free(false);
-			}
-
-			void hashmap_free(int free_entries) {
-				if (!table) {
-					return;
-				}
-				if (free_entries) {
-					struct hashmap_entry *e;
-					MapInterator iter(this);
-					while ((e = iter.hashmap_iter_next())) {
-						std::free(e);
-					}
-				}
-				std::free(table);
-				table = nullptr;
-				size = 0;
-				tablesize = 0;
-				//memset(map, 0, sizeof(*map));
-			}
-
-			int entry_equals(const struct hashmap_entry<T> *e1, const struct hashmap_entry<T> *e2,
-					const void *keydata) {
-				return (e1 == e2) || (e1->hash == e2->hash && !cmpfn(e1, e2, keydata));
-			}
-
-			unsigned int bucket(const struct hashmap_entry<T> *key) {
-				return key->hash & (tablesize - 1);
-			}
-			
-			void rehash(unsigned int newsize) {
-				unsigned int i, oldsize = tablesize;
-				struct hashmap_entry **oldtable = table;
-
-				alloc_table(newsize);
-				for (i = 0; i < oldsize; i++) {
-					struct hashmap_entry *e = oldtable[i];
-					while (e) {
-						struct hashmap_entry *next = e->next;
-						unsigned int b = bucket(e);
-						e->next = table[b];
-						table[b] = e;
-						e = next;
-					}
-				}
-				std::free(oldtable);
-			}
-
-			struct hashmap_entry **find_entry_ptr(const struct hashmap_entry *key, const void *keydata) {
-				struct hashmap_entry **e = &table[bucket(key)];
-				while (*e && !entry_equals(*e, key, keydata)) {
-					e = &(*e)->next;
-				}
-				return e;
-			}
-
-			hashmap_entry *hashmap_get(const hashmap_entry *key, const void *keydata) {
-				return *find_entry_ptr(key, keydata);
-			}
-
-			hashmap_entry *hashmap_get_from_hash(unsigned int hash, const void *keydata) {
-				struct hashmap_entry key;
-				key.hash = hash;
-				key.next = nullptr;
-				return hashmap_get(&key, keydata);
-			}
-
-			hashmap_entry *hashmap_get_next(const struct hashmap_entry *entry) {
-				hashmap_entry *e = entry->next;
-				for (; e; e = e->next) {
-					if (entry_equals((hashmap_entry*) entry, e, NULL)) {
-						return e;
-					}
-				}
-				return nullptr;
-			}
-
-			void hashmap_add(hashmap_entry *entry) {
-				unsigned int b = bucket(entry);
-
-				/* add entry */
-				entry->next = table[b];
-				table[b] = entry;
-
-				/* fix size and rehash if appropriate */
-				size++;
-				if (size > grow_at) {
-					rehash(tablesize << HASHMAP_RESIZE_BITS);
-				}
-			}
-
-			hashmap_entry* hashmap_remove(const hashmap_entry *key, const void *keydata) {
-				struct hashmap_entry *old;
-				struct hashmap_entry **e = find_entry_ptr(key, keydata);
-				if (!*e) {
-					return NULL;
-				}
-
-				/* remove existing entry */
-				old = *e;
-				*e = old->next;
-				old->next = NULL;
-
-				/* fix size and rehash if appropriate */
-				size--;
-				if (size < shrink_at) {
-					rehash(tablesize >> HASHMAP_RESIZE_BITS);
-				}
-				return old;
-			}
-
-			hashmap_entry *hashmap_put(hashmap_entry *entry) {
-				struct hashmap_entry *old = hashmap_remove(entry, NULL);
-				hashmap_add(entry);
-				return old;
-			}
-
-
 			/*
 			 * Iterator 
 			 */
 			class MapInterator {
 				public:
 					// hashmap_iter_init
-					MapInterator(LinkedHashMap* _map): map(_map), next(nullptr), tablepos(0) {
+					MapInterator(LinkedHashMap<T>* _map): map(_map), nextEntry(nullptr), tablepos(0) {
 					}
-					
-					hashmap_entry *hashmap_iter_next() {
-						struct hashmap_entry *current = next;
+
+					/*
+					 * hashmap_iter_next
+					 */	
+					hashmap_entry<T> *getNext() {
+						hashmap_entry<T> *current = nextEntry;
 						for (;;) {
 							if (current) {
-								next = current->next;
+								nextEntry = current->next;
 								return current;
 							}
 
@@ -266,11 +153,160 @@ namespace cache {
 						next = nullptr;
 					}
 
+					/* 
+					 * hashmap_iter_first
+					 */ 
+					hashmap_entry<T> *firstIter() {
+						reset();
+						return getNext();
+					}
+	
 				public:
+					hashmap_entry<T> *nextEntry;
+
+				private:
 					LinkedHashMap<T> *map;
-					struct hashmap_entry<T> *next;
 					unsigned int tablepos;
 			};
+
+			~LinkedHashMap() {
+				hashmap_free(false);
+			}
+
+			void hashmap_free(bool free_entries) {
+				if (!table) {
+					return;
+				}
+				if (free_entries) {
+					hashmap_entry<T> *e;
+					MapInterator iter(this);
+					while ((e = iter.getNext())) {
+						std::free(e);
+					}
+				}
+				std::free(table);
+				table = nullptr;
+				size = 0;
+				tablesize = 0;
+				//memset(map, 0, sizeof(*map));
+			}
+
+			int entry_equals(const hashmap_entry<T> *e1, const struct hashmap_entry<T> *e2,
+					const void *keydata) {
+				return (e1 == e2) || (e1->hash == e2->hash && !cmpfn(e1, e2, keydata));
+			}
+
+			unsigned int bucket(unsigned int hash) {
+				return hash & (tablesize - 1);
+			}
+			
+			void rehash(unsigned int newsize) {
+				unsigned int i, oldsize = tablesize;
+				hashmap_entry<T> **oldtable = table;
+
+				alloc_table(newsize);
+				for (i = 0; i < oldsize; i++) {
+					struct hashmap_entry<T> *e = oldtable[i];
+					while (e) {
+						hashmap_ent<T> *next = e->next;
+						unsigned int b = bucket(e->hash);
+						e->next = table[b];
+						table[b] = e;
+						e = next;
+					}
+				}
+				std::free(oldtable);
+			}
+
+			hashmap_entry<T> **find_entry_ptr(const hashmap_entry<T> *key, const void *keydata) {
+				hashmap_entry<T> **e = &table[bucket(key->hash)];
+				while (*e && !entry_equals(*e, key, keydata)) {
+					e = &(*e)->next;
+				}
+				return e;
+			}
+
+			/*
+			 * hashmap_get
+			 */
+			hashmap_entry<T> *getEntry(const hashmap_entry<T> *key, const void *keydata) {
+				return *find_entry_ptr(key, keydata);
+			}
+
+			hashmap_entry<T> *hashmap_get_from_hash(unsigned int hash, const void *keydata) {
+				hashmap_entry<T> key;
+				key.hash = hash;
+				key.next = nullptr;
+				return getEntry(&key, keydata);
+			}
+
+			hashmap_entry<T> *hashmap_get_next(const hashmap_entry<T> *entry) {
+				hashmap_entry<T> *e = entry->next;
+				for (; e; e = e->next) {
+					if (entry_equals(entry, e, nullptr)) {
+						return e;
+					}
+				}
+				return nullptr;
+			}
+
+			/*
+			 * hashmap_add
+			 */
+			void add(unsigned int hash, const T& value) {
+				unsigned int b = bucket(hash);
+
+				hashmap_entry<T>* entry = (hashmap_entry<T>*) std::malloc(sizeof(hashmap_entry));
+                if (entry == NULL) {
+                    throw std::runtime_error(std::strerror(errno));
+                }
+				entry->hash = hash;
+				std::memcpy(entry->value, arr, amount);
+
+				/* add entry */
+				entry->next = table[b];
+				table[b] = entry;
+
+				/* fix size and rehash if appropriate */
+				size++;
+				if (size > grow_at) {
+					rehash(tablesize << HASHMAP_RESIZE_BITS);
+				}
+			}
+
+			/*
+			 * hashmap_remove
+			 */
+			hashmap_entry<T>* remove(const hashmap_entry<T> *key, const void *keydata) {
+				hashmap_entry<T> *old;
+				hashmap_entry<T> **e = find_entry_ptr(key, keydata);
+				if (!*e) {
+					return nullptr;
+				}
+
+				/* remove existing entry */
+				old = *e;
+				*e = old->next;
+				old->next = nullptr;
+
+				/* fix size and rehash if appropriate */
+				size--;
+				if (size < shrink_at) {
+					rehash(tablesize >> HASHMAP_RESIZE_BITS);
+				}
+				return old;
+			}
+
+			/*
+			 * hashmap_put
+			 */
+			hashmap_entry<T>* put(hashmap_entry<T>* entry) {
+				hashmap_entry<T>* old = remove(entry, nullptr);
+				add(entry);
+				return old;
+			}
+
+			
 
 			static int always_equal(const void *unused1, const void *unused2, const void *unused3) {
 				return 0;
@@ -304,41 +340,36 @@ namespace cache {
 
 	};
 
-	/* hashmap_iter functions */
-	static inline hashmap_entry *hashmap_iter_first(LinkedHashMap::MapInterator *iter) {
-		iter->reset();
-		return iter->hashmap_iter_next();
-	}
-
+	
 	/* string interning */
-	struct pool_entry : public hashmap_entry {
+	struct pool_entry {
 		size_t len;
 		unsigned char data[FLEX_ARRAY];
 	};
 
-	static int pool_entry_cmp(const struct pool_entry *e1,
-			const struct pool_entry *e2,
+	static int pool_entry_cmp(const struct hashmap_entry<pool_entry> *e1,
+			const struct hashmap_entry<pool_entry> *e2,
 			const unsigned char *keydata) {
-		return e1->data != keydata &&
-			(e1->len != e2->len || memcmp(e1->data, keydata, e1->len));
+		return e1->value.data != keydata &&
+			(e1->value.len != e2->value.len || memcmp(e1->value.data, keydata, e1->value.len));
 	}
 
 	const void *memintern(const void *data, size_t len) {
 		// global static string pool 
 		/* initialize string pool hashmap */
-		static LinkedHashMap map((hashmap_cmp_fn) pool_entry_cmp, 0);
+		static LinkedHashMap<pool_entry> map(pool_entry_cmp, 0);
 
 		//if (!map.tablesize)
 			//hashmap_init(&map, (hashmap_cmp_fn) pool_entry_cmp, 0);
 
 		/* lookup interned string in pool */
-		pool_entry key;
-		hashmap_entry_init(&key, memhash(data, len));
-		key.len = len;
-		pool_entry* e = (pool_entry*)map.hashmap_get(&key, data);
+		hashmap_entry<pool_entry> key(memhash(data, len)); 
+		//hashmap_entry_init(&key, memhash(data, len));
+		key.value.len = len;
+		hashmap_entry<pool_entry>* e = map.getEntry(&key, data);
 		if (!e) {
 			/* not found: create it */
-			FLEX_ALLOC_MEM(e, data, data, len);
+			//FLEX_ALLOC_MEM(e, data, data, len);
 			hashmap_entry_init(e, key.hash);
 			e->len = len;
 			map.hashmap_add(e);
