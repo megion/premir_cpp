cccb3d55616aaeb2ab070cadd7fc3f03d41826f2
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-04-14 19:44:59 +0300

    refactoring str

--- src/cache/str/strbuf-utils.cpp
+++ src/cache/str/strbuf-utils.cpp
@@ -28,7 +28,7 @@ namespace cache {
 			}
 		}
 		
-		void addbufPercentquote(StringBuffer *dst, const StringBuffer *src) {
+		void strbuf_addbufPercentquote(StringBuffer *dst, const StringBuffer *src) {
 			for (size_t i = 0; i < src->len; i++) {
 				if (src->buf[i] == '%') {
 					dst->addch('%');
@@ -37,11 +37,157 @@ namespace cache {
 			}
 		}
 
+		void strbuf_expand(StringBuffer *sb, const char *format, expand_fn_t fn, void *context) {
+			for (;;) {
+				const char *percent;
+				size_t consumed;
+
+				percent = strchrnul(format, '%');
+				sb->add(format, percent - format);
+				if (!*percent) {
+					break;
+				}
+				format = percent + 1;
+
+				if (*format == '%') {
+					sb->addch('%');
+					format++;
+					continue;
+				}
+
+				consumed = fn(sb, format, context);
+				if (consumed) {
+					format += consumed;
+				} else {
+					sb->addch('%');
+				}
+			}
+		}
+
 		//void addUniqueAbbrev(StringBuffer *sb, const unsigned char *sha1, int abbrev_len) {
 		//sb->grow(GIT_SHA1_HEXSZ + 1);
 		//int r = find_unique_abbrev_r(sb->buf + sb->len, sha1, abbrev_len);
 		//sb->setLen(sb->len + r);
 		//}
+		
+		void strbuf_addIndentedText(StringBuffer *sb, const char *text,
+			   	int indent, int indent2) {
+			if (indent < 0) {
+				indent = 0;
+			}
+			while (*text) {
+				const char *eol = strchrnul(text, '\n');
+				if (*eol == '\n') {
+					eol++;
+				}
+				sb->addchars(' ', indent);
+				sb->add(text, eol - text);
+				text = eol;
+				indent = indent2;
+			}
+		}
+
+		size_t strbuf_fread(StringBuffer *sb, ssize_t size, FILE *f) {
+			size_t oldalloc = sb->getAlloc();
+
+			sb->grow(size);
+			size_t res = fread(sb->buf + sb->len, 1, size, f);
+			if (res > 0) {
+				sb->setLen(sb->len + res);
+			} else if (oldalloc == 0) {
+				sb->release();
+			}
+			return res;
+		}
+
+		ssize_t strbuf_readFile(StringBuffer *sb, int fd, size_t hint) {
+			size_t oldlen = sb->len;
+			size_t oldalloc = sb->getAlloc();
+
+			sb->grow(hint ? hint : 8192);
+			for (;;) {
+				ssize_t want = sb->getAlloc() - sb->len - 1;
+				ssize_t got = read_in_full(fd, sb->buf + sb->len, want);
+
+				if (got < 0) {
+					if (oldalloc == 0) {
+						sb->release();
+					} else {
+						sb->setLen(oldlen);
+					}
+					return -1;
+				}
+				sb->len += got;
+				if (got < want) {
+					break;
+				}
+				sb->grow(8192);
+			}
+
+			sb->buf[sb->len] = '\0';
+			return sb->len - oldlen;
+		}
+
+		ssize_t strbuf_readFileByPath(StringBuffer *sb, const char *path, size_t hint) {
+			int fd = open(path, O_RDONLY);
+			if (fd < 0) {
+				return -1;
+			}
+			ssize_t rlen = strbuf_readFile(sb, fd, hint);
+			close(fd);
+			if (rlen < 0) {
+				return -1;
+			}
+
+			return rlen;
+		}
+
+		ssize_t strbuf_readOnce(StringBuffer *sb, int fd, size_t hint) {
+			sb->grow(hint ? hint : 8192);
+			ssize_t cnt = xread(fd, sb->buf + sb->len, sb->getAlloc() - sb->len - 1);
+			if (cnt > 0) {
+				sb->setLen(sb->len + cnt);
+			}
+			return cnt;
+		}
+
+		/*
+		 * strbuf_write
+		 */
+		ssize_t strbuf_write(StringBuffer *sb, FILE *f) {
+			return sb->len ? fwrite(sb->buf, 1, sb->len, f) : 0;
+		}
+
+		/*
+		 * strbuf_readlink
+		 */
+		int strbuf_readlink(StringBuffer *sb, const char *path, size_t hint) {
+			size_t oldalloc = sb->getAlloc();
+
+			if (hint < 32) {
+				hint = 32;
+			}
+
+			while (hint < STRBUF_MAXLINK) {
+				sb->grow(hint);
+				int newLen = readlink(path, sb->buf, hint);
+				if (newLen < 0) {
+					if (errno != ERANGE) {
+						break;
+					}
+				} else if (newLen < hint) {
+					sb->setLen(newLen);
+					return 0;
+				}
+
+				/* .. the buffer was too small - try again */
+				hint *= 2;
+			}
+			if (oldalloc == 0) {
+				sb->release();
+			}
+			return -1;
+		}
 
 	}
 }
--- src/cache/str/strbuf-utils.h
+++ src/cache/str/strbuf-utils.h
@@ -18,7 +18,7 @@ namespace cache {
 		int is_rfc3986_unreserved(char ch);
 
 		bool starts_with(const char *str, const char *prefix); 
-		void addbufPercentquote(StringBuffer *dst, const StringBuffer *src);	
+		void strbuf_addbufPercentquote(StringBuffer *dst, const StringBuffer *src);	
 
 		//void addUniqueAbbrev(const StringBuffer *a, const unsigned char *sha1, int abbrev_len);
 
@@ -30,6 +30,59 @@ namespace cache {
 		//}
 		//return a->len < b->len ? -1: a->len != b->len;
 		//}
+		
+		/**
+		 * This function can be used to expand a format string containing
+		 * placeholders. To that end, it parses the string and calls the specified
+		 * function for every percent sign found.
+		 *
+		 * The callback function is given a pointer to the character after the `%`
+		 * and a pointer to the struct strbuf.  It is expected to add the expanded
+		 * version of the placeholder to the strbuf, e.g. to add a newline
+		 * character if the letter `n` appears after a `%`.  The function returns
+		 * the length of the placeholder recognized and `strbuf_expand()` skips
+		 * over it.
+		 *
+		 * The format `%%` is automatically expanded to a single `%` as a quoting
+		 * mechanism; callers do not need to handle the `%` placeholder themselves,
+		 * and the callback function will not be invoked for this placeholder.
+		 *
+		 * All other characters (non-percent and not skipped ones) are copied
+		 * verbatim to the strbuf.  If the callback returned zero, meaning that the
+		 * placeholder is unknown, then the percent sign is copied, too.
+		 *
+		 * In order to facilitate caching and to make it possible to give
+		 * parameters to the callback, `strbuf_expand()` passes a context pointer,
+		 * which can be used by the programmer of the callback as she sees fit.
+		 */
+		typedef size_t (*expand_fn_t) (StringBuffer *sb, const char *placeholder,
+				void *context);
+		void strbuf_expand(StringBuffer *sb, const char *format, expand_fn_t fn, void *context); 
+
+		//size_t expand_dict_cb(const char *placeholder, void *context) {
+		//strbuf_expand_dict_entry *e = context;
+		//size_t len;
+
+		//for (; e->placeholder && (len = std::strlen(e->placeholder)); e++) {
+		//if (!strncmp(placeholder, e->placeholder, len)) {
+		//if (e->value) {
+		//addstr(e->value);
+		//}
+		//return len;
+		//}
+		//}
+		//return 0;
+		//}
+		
+		void strbuf_addIndentedText(StringBuffer *sb, const char *text, int indent, int indent2);
+
+		
+		size_t strbuf_fread(StringBuffer *sb, ssize_t size, FILE *f); 
+		ssize_t strbuf_readFile(StringBuffer *sb, int fd, size_t hint);
+		ssize_t strbuf_readFileByPath(StringBuffer *sb, const char *path, size_t hint);
+		ssize_t strbuf_readOnce(StringBuffer *sb, int fd, size_t hint);
+		ssize_t strbuf_write(StringBuffer *sb, FILE *f);
+		int strbuf_readlink(StringBuffer *sb, const char *path, size_t hint);
 
 	}
 }
