91ad4da271c115040aca18b0637e618d10820271
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-04-25 23:00:11 +0300

    string list test

--- test/cache/StringListTest.cpp
+++ test/cache/StringListTest.cpp
@@ -62,61 +62,18 @@ void test_remove_duplicates() {
 void test_sort_list() {
     cache::StringList<bool> list(nullptr, false);
     cache::str::StringBuffer sb;
-    cache::StringList<bool>::ListItem* item;
 
     // strbuf_read(&sb, 0, 0);
     sb.addstr("abc\ndefg\nhij\ndefg\n");
 
-    // list.print();
-    //std::printf("sb %s\n", sb.buf);
     if (sb.buf[sb.len - 1] == '\n') {
         sb.setLen(sb.len - 1);
-	}
-    //std::printf("sb %s\n", sb.buf);
+    }
     list.splitInPlace(sb.buf, '\n', -1);
-	list.sort();
+    list.sort();
     assert(std::strcmp(list[1].string, "defg") == 0);
     assert(std::strcmp(list[2].string, "defg") == 0);
-    // char string[] = "abc,defg,hij";
-    // list.splitInPlace(string, ',', 1);
-    // assert(list.size() == 2);
-    // assert(std::strcmp(list[0].string, "abc") == 0);
-    // assert(std::strcmp(list[1].string, "defg,hij") == 0);
-    // list.clear(false);
 }
-
-// int cmd_main(int argc, const char **argv)
-//{
-
-// if (argc == 2 && !strcmp(argv[1], "sort")) {
-// struct string_list list = STRING_LIST_INIT_NODUP;
-// struct strbuf sb = STRBUF_INIT;
-// struct string_list_item *item;
-
-// strbuf_read(&sb, 0, 0);
-
-/*
- * Split by newline, but don't create a string_list item
- * for the empty string after the last separator.
- */
-// if (sb.buf[sb.len - 1] == '\n')
-// strbuf_setlen(&sb, sb.len - 1);
-// string_list_split_in_place(&list, sb.buf, '\n', -1);
-
-// string_list_sort(&list);
-
-// for_each_string_list_item(item, &list)
-// puts(item->string);
-
-// string_list_clear(&list, 0);
-// strbuf_release(&sb);
-// return 0;
-//}
-
-// fprintf(stderr, "%s: unknown function name: %s\n", argv[0],
-// argv[1] ? argv[1] : "(there was none)");
-// return 1;
-//}
 
 void stringList_test() {
     suite("StringList");
